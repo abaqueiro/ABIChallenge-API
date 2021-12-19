225075d53e46daef71e6fc27e966ee8eee7b0c8a
Alfonso Baqueiro Bernal <abaqueiro@gmail.com>
2021-12-19 06:24:51 +0000

    No funciona muy bien, así que intentaremos otra aproximación.
    	modified:   web/main.py

--- web/main.py
+++ web/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import os
 
@@ -42,8 +42,12 @@ async def read_item( passenger_id: int ):
     cmd = "grep -e '^" + str(passenger_id) + ",' ../data/train.csv"
     stream = os.popen( cmd )
     out = stream.read()
+    if len(out)==0:
+        raise HTTPException(status_code=404, detail="Not found")
+    else:
+        a = out.split(',')
+        return { "out": out, "passenger_id": a[0], "pclass": a[1], "name": a[2], "sex": a[3], "age": a[4], "sibsp": a[5], "parch": a[6], "ticket": a[7], "fare": a[8], "cabin": a[9], "embarked": a[10] }
 
-    return { "passenger_id": passenger_id, "out": out }
 # delete
 #@app.delete("/trainingrecord/{passenger_id}")
 #async def read_item( passenger_id: int, survived: bool, pclass: int, name: str, sex: str, age: int, sibsp: int, parch: int, ticket: str, fare: float, cabin: str, embarked: str ):
